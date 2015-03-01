2b4a0202912ee1ca396fe18055b764e87c9fd689
Iain Ferguson <i.ferguson@idk.co.uk>
2015-03-01 17:47:04 +0000

    added

---
+++ database/MediaProerties.cpp
@@ -0,0 +1,21 @@
+/*
+ * MediaProerties.cpp
+ *
+ *  Created on: Jul 1, 2014
+ *      Author: wzw7yn
+ */
+
+#include "MediaProerties.h"
+
+namespace simplearchive {
+
+MediaProerties::MediaProerties() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MediaProerties::~MediaProerties() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace simplearchive */
---
+++ database/SQLBuilder.cpp
@@ -0,0 +1,21 @@
+/*
+ * SQLBuilder.cpp
+ *
+ *  Created on: Jun 13, 2014
+ *      Author: wzw7yn
+ */
+
+#include "SQLBuilder.h"
+
+//namespace simplearchive {
+
+SQLBuilder::SQLBuilder() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SQLBuilder::~SQLBuilder() {
+	// TODO Auto-generated destructor stub
+}
+
+//} /* namespace simplearchive */
