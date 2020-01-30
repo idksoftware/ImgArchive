229fed297e3fb40a543319eeed722e8abf8e6898
Iain Ferguson <iferguson@kirintec.com>
2020-01-30 17:56:41 +0000

    changed

--- iaserver/ServerApp.h
+++ iaserver/ServerApp.h
@@ -32,24 +32,6 @@
 **
 ** #$$@@$$# */
 
-/*
- Things to do??
- 1) A reqect list of all the items completly rejected i.e files that have file exetentions
- this are unknown
- 2) Dups rejected list.
- 3) Post process phase where images can be post processed.
- 3) Archive process phase. All images in the archive are visited for sum process to be applied.
- for example a new type of DNG format needs to be applied to all RAW images.
- or the databese need recreating.
- 4) if run in a working directory look for images in that directory??
- 5) make running without enviroment simple
- 6) Update the dups index with the seq id so that if a dup is found then an option to use the original
- seg id can be used.
-
-
-
-
- 6) create log file write points so the log is written during execution of processing.
 
 
  */
