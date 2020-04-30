68e39eb4b31acea642b9b472aaaee960f1d1780c
Iain Ferguson <i.ferguson@idk.co.uk>
2020-04-30 13:57:33 +0100

    added

---
+++ util/Base32.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+
+class Base32 {
+	std::string m_str;
+	unsigned long m_int;
+	void padToBase32(std::string& str, const size_t num)
+	{
+		if (num > str.size())
+			str.insert(0, num - str.size(), 'A');
+	}
+public:
+	Base32() {};
+	virtual ~Base32() {};
+	const char* decimal2Base32(long dec, int padding = 0);
+	int Base32ToDecimal(std::string encoded);
+	const char* getBase32() {
+		return m_str.c_str();
+	}
+};
+
+class Base32Hex {
+	std::string m_str;
+	unsigned long m_int;
+	void padToBase32(std::string& str, const size_t num)
+	{
+		if (num > str.size())
+			str.insert(0, num - str.size(), '0');
+	}
+public:
+	Base32Hex() {};
+	virtual ~Base32Hex() {};
+	const char* decimal2Base32(long dec, int padding = 0);
+	int Base32ToDecimal(std::string encoded);
+	const char* getBase32() {
+		return m_str.c_str();
+	}
+};
