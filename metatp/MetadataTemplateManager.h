f0aa6f2c63aa7f70088531f0b27bbdff85446800
Iain Ferguson <i.ferguson@idk.co.uk>
2020-06-19 13:29:07 +0100

    added

---
+++ metatp/MetadataTemplateManager.h
@@ -0,0 +1,40 @@
+#pragma once
+//class MetadataTemplateManager
+//{
+//};
+
+#include <map>
+#include <mutex>
+#include "ConfigReader.h"
+#include "ResultsPresentation.h"
+#include "MasterMetadataTemplate.h"
+#include "DerivativeMetadataTemplate.h"
+
+namespace simplearchive {
+
+	class MetadataTemplateManager {
+
+
+		static Config_ptr m_templateFile;
+		static std::string& getValue(const char* key);
+		static MasterMetadataTemplateRow m_metadataTemplateRow;
+		//static DerivativeMetadataRow m_derivativeMetadataRow;
+
+		MetadataTemplateManager() noexcept = default;
+	public:
+		static MetadataTemplateManager& GetInstance();
+		virtual ~MetadataTemplateManager();
+
+		MetadataTemplateManager(const MetadataTemplateManager& src) = delete;
+		MetadataTemplateManager& operator=(const MetadataTemplateManager& rhs) = delete;
+
+		static bool readMaster(const char* path, bool current);
+		static bool readDerivative(const char* path, bool current);
+
+		static bool readMaster(const char* path, const char* datafile);
+		static bool readDerivative(const char* path, const char* datafile);
+		static bool write(const char* datafile);
+		static MetadataObject_ptr getMetadataObject();
+	};
+
+}
