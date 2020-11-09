b17c9320f82fc62455ba1f8d96a285c9e8af5461
Iain Ferguson <i.ferguson@idk.co.uk>
2020-11-09 01:48:43 +0000

    changed

--- iaadmin/SetEnviromentVariables.cpp
+++ iaadmin/SetEnviromentVariables.cpp
@@ -20,8 +20,6 @@ enum class FolderOptions {
 };
 
 enum class EnabledOptions {
-
-	
 	UNKNOWN
 };
 
--- iaadmin/SetEnviromentVariables.h
+++ iaadmin/SetEnviromentVariables.h
@@ -10,6 +10,8 @@ class SetEnviromentVariables : public ParseOptions {
 	FolderOptions processFolders(std::string& optionString);
 	EnabledOptions processEnableOptions(std::string& optionString);
 	EnabledOptions processDisableOptions(std::string& optionString);
+	std::string m_option;
+	std::string m_value;
 public:
 	SetEnviromentVariables() = default;
 	virtual ~SetEnviromentVariables() = default;
